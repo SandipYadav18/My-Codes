0a8d25f862861f242e435395b19e4fb317daf4cc
SandipYadav18 <ssy852002@gmail.com>
2024-05-23 12:53:54 +0530

    Add files via upload

---
+++ Program2_4.c
@@ -0,0 +1,33 @@
+//  4. Accept two numbers from user and display first number in second number of times.
+
+#include<stdio.h>
+
+void Display(int iNo,int iFrequency)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 1;iCnt <= iFrequency; iCnt++)
+    {
+        printf("%d\n",iNo);
+    }
+}
+int main()
+{
+    int iValue = 0;
+    int iCount = 0;
+
+    printf("Enter number :\n");
+    scanf("%d",&iValue);
+
+    printf("Enter frequency :\n");
+    scanf("%d",&iCount);
+
+    Display(iValue, iCount);
+
+    return 0;
+}    
